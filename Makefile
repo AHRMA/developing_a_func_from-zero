2516810c7f838e4c531ad32fb5a51932bba2229a
AmazingAHRM <101900442+AHRMA@users.noreply.github.com>
2025-12-15 08:53:39 +0000

    did some testing functions

---
+++ Makefile
@@ -0,0 +1,13 @@
+install:
+	pip install --upgrade pip && \
+		pip install -r requirements.txt
+
+test:
+	python -m pytest -vv tests_hellop.py
+
+format:
+	black *.py
+lint:
+	pylint --disable=R,C hello.py
+
+add: install lint test
--- hello.py
+++ hello.py
@@ -1,8 +1,3 @@
-my_list = ["apple", "banana", "cherry"]
-my_dict = {"name": "Alice", "age": 30, "city": "New York"}
-
-for item in my_list:
-    print(f"This is my {item}.")
-for key, value in my_dict.items():
-    print(f"My {key} is {value}.")
-print("All items have been printed.")
+def add(x,y):
+    return x + y
+print(add(2,3))
