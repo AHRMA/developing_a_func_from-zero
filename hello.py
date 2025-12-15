479dc1a175ed343eda7fa1a0cb80ac0b8a49fb00
AmazingAHRM <101900442+AHRMA@users.noreply.github.com>
2025-12-15 08:07:12 +0000

    adding new files to rippo

---
+++ hello.py
@@ -0,0 +1,8 @@
+my_list = ["apple", "banana", "cherry"]
+my_dict = {"name": "Alice", "age": 30, "city": "New York"}
+
+for item in my_list:
+    print(f"This is my {item}.")
+for key, value in my_dict.items():
+    print(f"My {key} is {value}.")
+print("All items have been printed.")
